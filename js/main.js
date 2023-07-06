bb13879bc49a81b7e71b3c1d618a4596eed01d45
susumu1125 <susumu04301125@gmail.com>
2023-07-06 11:55:44 +0900

    追加しました

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// $(function(){
+//     $('.slide-photo').slick({
+//         accessibility: true,
+//         autoplay: true,
+//         dots: true,
+//         autoplaySpeed: 3000,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear',
+//         pauseOnHover: false,
+//         arrows: true,
+//         vertical: false
+//     });
+//    });
