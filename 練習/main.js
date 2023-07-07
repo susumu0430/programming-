87de25e9374fa1d618a5974b5e55892c91750fd3
susumu1125 <susumu04301125@gmail.com>
2023-07-07 14:27:38 +0900

    追加

--- 練習/main.js
+++ 練習/main.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const obj1 = {
-    name: "健",
+// const obj1 = {
+//     name: "健",
+//     age: 24,
+// };
+// console.log(obj1);
+
+// const obj1 = {
+//     name:"亮太",
+//     age: 24,
+// };
+// obj1.name ="Nushida";
+// console.log(obj1);
+
+const obj1 ={
+    name:"涼",
     age: 24,
 };
+obj1.name = "Nushida";
+obj1.address ="Tokyo";
+console.log(obj1);
