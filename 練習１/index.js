96d77dc880714d1a0d89fc495cf57c66a3de8fa3
susumu1125 <susumu04301125@gmail.com>
2023-07-08 14:56:22 +0900

    追加

--- 練習１/index.js
+++ 練習１/index.js
@@ -2,8 +2,36 @@
 
 const inoki = ['いーち','にーい','さーん','ダー！！'];
 
-let index = 0;
-while(index < inoki.length) {
-    console.log(inoki[index]);
-  index++;
-}
+// let index = 0;
+// while(index < inoki.length) {
+//     console.log(inoki[index]);
+//   index++;
+// }
+
+// if(inoki.length > 4) {
+//   console.log("ボンバイエ！");
+// } else {
+//     console.log("ボンバ！");
+// }
+
+const test = (arg) => {
+    if(inoki.length > arg) {
+        console.log("ボンバイエ！");
+      } else {
+          console.log("ボンバ！");
+      }
+};
+
+const test2 = {
+    color: "pink",
+    size: "large",
+    purfume:"mint",
+    goTo: () => {
+        console.log("Hello");
+    }
+};
+
+// console.log(document.getElementsByTagName("button")[1]);
+document.getElementsByTagName("button")[0].addEventListener("click",() => {
+    window.alert("Hi");
+});
