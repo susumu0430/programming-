975b2220f163078bec51fecbd6a34cf83ee11f0a
susumu1125 <susumu04301125@gmail.com>
2023-07-10 14:50:31 +0900

    追加

--- 練習１/index.js
+++ 練習１/index.js
@@ -1,47 +1,75 @@
 'use strict';
 
-const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？';
-const answers = [
-    'スーパーファミコン',
-    'プレイステーション２','ニンテンドースイッチ',
-    'ニンテンドーDS'
+const quiz = [
+    {
+      question:'ゲーム市場、最も売れたゲーム機は次の内どれ？',
+      answers: [
+        'スーパーファミコン',
+        'プレイステーション２',
+        'ニンテンドースイッチ',
+        'ニンテンドーDS'
+      ],
+      correct:'ニンテンドーDS'
+    },{
+        question:'糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+      answers: [
+        'MOTHER2',
+        'スーパーマリオブラザーズ３',
+        'スーパードンキーコング',
+        '星のカービィ'
+      ],
+      correct:'MOTHER2'
+    },{
+        question:'ファイナルファンタジーIVの主人公の名前は？',
+      answers: [
+        'フリオニール',
+        'クラウド',
+        'ティーダ',
+        'セシル'
+      ],
+      correct:'セシル'
+    }
 ];
-const correct = 'ニンテンドーDS';
 
-console.log();
-document.getElementById('js-question').textContent = question;
+const quizLength = quiz.length;
+let quizIndex = 0;
+
 
 const $button = document.getElementsByTagName('button');
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+const buttonLength = $button.length;
+//クイズ問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent =quiz[quizIndex].question;
+    let buttonIndex =0;
+    while(buttonIndex < buttonLength){
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex]
+        buttonIndex++;
+    }
+}
+setupQuiz();
 
-$button[0].addEventListener('click',() => {
-  if(correct === $button[0].textContent) {
-    window.alert('正解');
-  } else {
-    window.alert('不正解');
-  }
-});
-$button[1].addEventListener('click',() => {
-    if(correct === $button[1].textContent) {
-      window.alert('正解');
-    } else {
-      window.alert('不正解');
-    }
-  });
-  $button[2].addEventListener('click',() => {
-    if(correct === $button[2].textContent) {
-      window.alert('正解');
-    } else {
-      window.alert('不正解');
-    }
-  });
-  $button[3].addEventListener('click',() => {
-    if(correct === $button[3].textContent) {
-      window.alert('正解');
-    } else {
-      window.alert('不正解');
-    }
-  });
+const clickHandler = (e) => {
+if(quiz[quizIndex].correct === e.target.textContent) {
+  window.alert('正解');
+} else {
+  window.alert('不正解');
+}
+
+quizIndex++;
+
+if(quizIndex < quizLength) {
+  setupQuiz();
+} else {
+  window.alert('終了');
+}
+};
+
+//ボタンをクリックしたら正誤判定
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click',(e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
+
