341193c77eb9cef9d2fa1ce1602b480f035dd479
susumu1125 <susumu04301125@gmail.com>
2023-07-14 14:41:17 +0900

    追加

--- 練習２/main.js
+++ 練習２/main.js
@@ -5,6 +5,39 @@
 const $doc = document;
 const $tab = $doc.getElementById('js-tab');
 const $nav = $tab.querySelectorAll("[data-nav]");
+const $content = $tab.querySelectorAll("[data-content]");
+//初期化
+const init = () => {
+    $content[0].style.display = "block";
+};
+init();
+//クリックイベント
+const handleClick = (e) => {
+    e.preventDefault();
+//クリックされたnav とそのdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
 
+    console.log("targetVal", targetVal);
+
+//対象外のnav. content全て一旦リセットする
+let index =0;
+while(index < $nav.length){
+    $content[index].style.display ="none";
+    index++;
+}
+
+//対象のコンテンツをアクティブ化
+$tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+$nav[targetVal].classList.add('is-active');
+console.log('$nav[targetVal].classList',$nav[targetVal].classList);
+};
+
+//全nav要素に対して関数を適応 発火
+let index = 0;
+while(index < $nav.length){
+    $nav[index].addEventListener("click", (e) => handleClick(e));
+    index++;
+}
 
 })();
