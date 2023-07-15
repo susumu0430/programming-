930a2513b555a5dc00299b7c430ca69058cbe682
susumu1125 <susumu04301125@gmail.com>
2023-07-15 14:46:16 +0900

    追加

--- 練習２/main.js
+++ 練習２/main.js
@@ -7,6 +7,7 @@ const $tab = $doc.getElementById('js-tab');
 const $nav = $tab.querySelectorAll("[data-nav]");
 const $content = $tab.querySelectorAll("[data-content]");
 const ACTIVE_CLASS= 'is-active';
+const navLen = $nav.length;
 //初期化
 const init = () => {
     $content[0].style.display = "block";
@@ -23,7 +24,7 @@ const handleClick = (e) => {
 
 //対象外のnav. content全て一旦リセットする
 let index =0;
-while(index < $nav.length){
+while(index < navLen){
     $content[index].style.display ="none";
     $nav[index].classList.remove(ACTIVE_CLASS);
     index++;
@@ -38,7 +39,7 @@ console.log('$nav[targetVal].classList',$nav[targetVal].classList);
 
 //全nav要素に対して関数を適応 発火
 let index = 0;
-while(index < $nav.length){
+while(index < navLen){
     $nav[index].addEventListener("click", (e) => handleClick(e));
     index++;
 }
