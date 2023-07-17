fa61fa3d333a8875d2925edec49d9fc31f9e5896
susumu1125 <susumu04301125@gmail.com>
2023-07-17 14:54:59 +0900

    追加

---
+++ 練習 3/main.js
@@ -0,0 +1,40 @@
+// 'use strict';
+
+(() => {
+
+    class Accordion {
+        //初期化
+        constructor (obj) {
+
+        console.log('obj', obj.hookName)
+
+            const $elm = document.querySelector('#js-accordion');
+            const $trigger = $elm.getElementsByTagName('a');
+         
+            const $triggerLen = $trigger.length;
+            let index =0;
+            while (index <  $triggerLen) {
+             $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+             index++;
+            }         
+        }
+       //クリックしたら実行される
+        clickHandler(e) {
+            e.preventDefault();
+            
+            const $target = e.currentTarget;
+            const $content =$target.nextElementSibling;
+            
+            if($content.style.display === 'block') {
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        }   
+    }
+
+    const fuckingAccordion = new Accordion({
+        hookName: '#js-faq',
+        tagName: 'p'
+    });
+})(); 
