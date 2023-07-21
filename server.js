e96920855141df30b53de95ec10958875abff22a
susumu1125 <susumu04301125@gmail.com>
2023-07-21 13:50:16 +0900

    追加

--- server.js
+++ server.js
@@ -2,15 +2,26 @@
 
 const http = require("http");
 const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
 
 //webサーバーを作ろう
 const server = http.createServer((req, res) => {
     //ブラウザからアクセスが来た時の処理
     res.writeHead(200, {"Content-Type": "text/html"})
-    res.write("<h1>Hello World</h1>");
+    res.write(html);
     res.end();
+
+    if(req.method == "GET") {
+
+    }
+    
+    if(req.method == "POST") {
+
+}
 });
+
+
 
 server.listen(PORT, () => {
     console.log("server running");
-});
+}); 
