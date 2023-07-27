c59e64cf497b6ea50cf605c75cb3ac9cb0fdd6eb
susumu1125 <susumu04301125@gmail.com>
2023-07-27 14:15:56 +0900

    追加する

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,28 @@
+import { useState, useRef} from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  return <div className="App">
-    <TodoList/>
-    <input type="text"/>
-    <button>タスクを追加</button>
+  const [todos, setTodos] = useState([{
+    id: 1, name:"Todo1",completed: false},
+  ]);
+
+const todoNameRef = useRef();
+
+const handleAddTodo = () => {
+  //タスクを追加する
+  console.log(todoNameRef);
+
+};
+
+  return (
+  <div className="App">
+    <TodoList todos={todos}/>
+    <input type="text" ref={todoNameRef}/>
+    <button onAbort={handleAddTodo}>タスクを追加</button>
     <button>完了したタスクの削除</button>
+    <div>残りのタスク:0</div>
   </div>
+  );
 }
 
 export default App;
--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,9 +1,7 @@
 import React from 'react'
+import Todo from './Todo';
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
-}
-
+const TodoList = ({todos}) => {
+  return todos.map((todo) => <Todo todo={todo} key={todo}/>);
+};
 export default TodoList;
