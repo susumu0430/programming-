f0dc4dc9dd05c59743136639a14b72b65de8649d
susumu1125 <susumu04301125@gmail.com>
2023-07-28 10:52:53 +0900

    追加する

--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -11,4 +11,4 @@ const Todo = ({todo}) => {
   )
 }
 
-export default Todo
+export default Todo;
--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -2,6 +2,6 @@ import React from 'react'
 import Todo from './Todo';
 
 const TodoList = ({todos}) => {
-  return todos.map((todo) => <Todo todo={todo} key={todo}/>);
+  return todos.map((todo) => <Todo todo={todo} key={todo.id}/>);
 };
 export default TodoList;
