1e935a224750f5f4ca3584ee75118826164f17fb
susumu1125 <susumu04301125@gmail.com>
2023-07-28 14:12:13 +0900

    追加

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,19 +12,30 @@ const todoNameRef = useRef();
 const handleAddTodo = () => {
   //タスクを追加する
   const name = todoNameRef.current.value;
+  if(name === "")return;
   setTodos((prevTodos) => {
     return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
   });
   todoNameRef.current.value =null;
 };
 
+const toggleTodo = (id) => {
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id ===id);
+  todo.completed = !todo.completed;
+  setTodos(newTodos);
+}
+const handleClear = () => {
+  const newTodos = todos.filter((todo) => !todo.completed);
+  setTodos(newTodos);
+};
   return (
   <div className="App">
-    <TodoList todos={todos}/>
+    <TodoList todos={todos} toggleTodo={toggleTodo}/>
     <input type="text" ref={todoNameRef}/>
     <button onClick={handleAddTodo}>タスクを追加</button>
-    <button>完了したタスクの削除</button>
-    <div>残りのタスク:0</div>
+    <button onClick={handleClear}>完了したタスクの削除</button>
+    <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
   </div>
   );
 }
--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({todo,toggleTodo}) => {
+
+const handleTodoClick= () => {
+  toggleTodo(todo.id);
+}
+
   return (
     <div>
         <label>
-            <input type='checkbox' checked={todo.completed}readOnly />
+            <input 
+            type='checkbox' 
+            checked={todo.completed}
+            readOnly 
+            onChange={handleTodoClick} />
         </label>
        {todo.name}
        </div>
