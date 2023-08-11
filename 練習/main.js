0fb58e1df0497d425e882ee99c8854e7502637e9
susumu1125 <susumu04301125@gmail.com>
2023-08-11 14:50:57 +0900

    追加

--- 練習/main.js
+++ 練習/main.js
@@ -1,10 +1,8 @@
 
-const myPyofile = {
-    name:"じゃんけぇ",
-    age: 28,
-};
-const message1 =`名前は${myPyofile.name}です。年齢は${myPyofile.age}歳です`;
-console.log(message1);
-const { name, age } = myPyofile;
-const message2 =`名前は${name}です。年齢は${age}歳です`;
-console.log(message2);
+const nameArr = ["田中","山田","山崎"];
+// const nameArr2 = nameArr.map((name)=> {
+// return name;
+// })
+// console.log(nameArr2);
+
+nameArr.map((name) => console.log(name));
