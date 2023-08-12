f5ad1cd8098184038aaedcf40afcec478913dc4b
susumu1125 <susumu04301125@gmail.com>
2023-08-12 13:44:56 +0900

    追加

--- 練習/main.js
+++ 練習/main.js
@@ -1,5 +1,5 @@
 
-const nameArr = ["田中","山田","山崎"];
+// const nameArr = ["田中","山田","山崎"];
 // for (let index = 0; index < nameArr.length; index++) {
 //     console.log(`${index + 1}番目は${nameArr[index]}です`)
 // }
@@ -24,3 +24,34 @@ const nameArr = ["田中","山田","山崎"];
 //    }
 // })
 // console.log(newNumArr);
+
+//三項演算子
+//ある条件? 条件がtrueの時 : 条件がfalseの時
+const val1 = 1 < 0 ? "true" : "false"
+console.log(val1);
+// const num = "1555";
+// // console.log(num.toLocaleString());
+
+// const fomattedNaum = typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(fomattedNaum);
+const checkedSum = (num1, num2) => {
+   return num1 + num2 > 100 ? "100を超えています" : "許容範囲です"
+}
+console.log(checkedSum(50, 40));
+console.log(checkedSum(60, 60));
+
+
+// || は左側がfalseなら右側を返す
+const num = 10;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+const false1 = true;
+const false2 = false;
+if(false1 || false2) {
+   console.log("フラグ")
+}
+// && は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
