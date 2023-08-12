6c5df96e4e6d4dc29708e8c3d8a59da185797301
susumu1125 <susumu04301125@gmail.com>
2023-08-12 10:57:19 +0900

    ty

--- 練習/main.js
+++ 練習/main.js
@@ -1,8 +1,26 @@
 
 const nameArr = ["田中","山田","山崎"];
+// for (let index = 0; index < nameArr.length; index++) {
+//     console.log(`${index + 1}番目は${nameArr[index]}です`)
+// }
 // const nameArr2 = nameArr.map((name)=> {
 // return name;
 // })
 // console.log(nameArr2);
 
-nameArr.map((name) => console.log(name));
+// nameArr.map((name , index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//     return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNumArr = nameArr.map((name) => {
+//    if (name === "山崎") {
+//     return name
+//    } else {
+//     return `${name}さん`
+//    }
+// })
+// console.log(newNumArr);
