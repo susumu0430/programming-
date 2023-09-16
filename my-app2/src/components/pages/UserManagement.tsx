eb02f0bd02ffd2a912ea2d2f530f1bbbab0a5e2c
susumu1125 <susumu04301125@gmail.com>
2023-09-16 11:50:51 +0900

    追加

--- my-app2/src/components/pages/UserManagement.tsx
+++ my-app2/src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
 import {memo,FC} from"react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
+
 
 export const UserManagement :FC=memo(()=> {
-    return<p>ユーザー管理ページです</p>
+    return(
+        <Wrap p={{ base: 4, md: 10 }}>
+           <WrapItem>
+           <UserCard imageUrl="https://source.unsplash.com/random" userName="ジョウダン" fullName="Tanaka Masato"/>
+           </WrapItem> 
+        </Wrap>
+    )
 })
