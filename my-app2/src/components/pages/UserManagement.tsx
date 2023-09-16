63464d1ea8fc00a2b1c3d6492eb78467e281bdc8
susumu1125 <susumu04301125@gmail.com>
2023-09-16 13:55:19 +0900

    追加

--- my-app2/src/components/pages/UserManagement.tsx
+++ my-app2/src/components/pages/UserManagement.tsx
@@ -1,14 +1,31 @@
-import {memo,FC} from"react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { UserCard } from "../organisms/user/UserCard";
+/*eslint-disable react-hooks/exhaustive-deps*/
+import {memo,FC, useEffect} from"react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers }from "../../hooks/useAllUsers"
 
 export const UserManagement :FC=memo(()=> {
+    const {getUsers, users ,loading} = useAllUsers()
+
+useEffect(() => getUsers(),[])
+    
     return(
-        <Wrap p={{ base: 4, md: 10 }}>
-           <WrapItem>
-           <UserCard imageUrl="https://source.unsplash.com/random" userName="ジョウダン" fullName="Tanaka Masato"/>
-           </WrapItem> 
-        </Wrap>
+        <>
+        {loading ?(
+        <Center h="100vh">
+           <Spinner />
+        </Center>
+        ):(
+            <Wrap p={{ base: 4, md: 10 }}>
+                {users.map((user) => (
+               <WrapItem key={user.id} mx="auto">
+               <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name}/>
+               </WrapItem> 
+                ))}
+         </Wrap>
+        ) }
+        </>
     )
 })
+    
