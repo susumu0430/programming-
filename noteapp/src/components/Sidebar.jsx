824b06faebbda32be5c9cb3a7b40d66108b9895c
susumu1125 <susumu04301125@gmail.com>
2023-10-09 10:25:11 +0900

    追加しました

--- noteapp/src/components/Sidebar.jsx
+++ noteapp/src/components/Sidebar.jsx
@@ -9,24 +9,24 @@ const Sidebar = ({onAddNote, notes}) => {
             <button onClick={onAddNote}>追加</button>
         </div>
         <div className="app-sidebar-notes">
-            {notes.map((note) => (
-            //     <div className="app-sidebar-note">
-            //     <div className="app-sidebar-note-title">
-            //         <strong>タイトル</strong>
-            //         <button>削除</button>
-            //     </div>
-            //     <p>ノートの内容です</p>
-            //     <small>最後の修正日:xx</small>
-            // </div>
-            ))}
-            <div className="app-sidebar-note">
+           {notes.map((note) => (
+          <div className="app-sidebar-note">
+          <div className="app-sidebar-note-title">
+              <strong>タイトル</strong>
+              <button>削除</button>
+          </div>
+          <p>ノートの内容です</p>
+          <small>最後の修正日:xx</small>
+          </div>
+           ))}
+            {/* <div className="app-sidebar-note">
                 <div className="app-sidebar-note-title">
                     <strong>タイトル</strong>
                     <button>削除</button>
                 </div>
                 <p>ノートの内容です</p>
                 <small>最後の修正日:xx</small>
-            </div>
+            </div> */}
         </div>
     </div>
   )
