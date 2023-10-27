8dd682e88609735fb808c4d33e0f14d80509e98d
susumu1125 <susumu04301125@gmail.com>
2023-10-27 14:55:23 +0900

    追加

--- portfolio/src/components/BlogPage.jsx
+++ portfolio/src/components/BlogPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className='container'>
+        <h1>ブログ</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sed! Enim fugit fuga, sint et voluptas, ipsum cum tempore praesentium illo maxime obcaecati dignissimos delectus hic asperiores assumenda, laboriosam aperiam nostrum? Qui, error dignissimos deserunt facilis explicabo tenetur, numquam harum recusandae eum quae quam possimus dolore libero aut incidunt quas. Harum optio inventore blanditiis molestias iure nemo, veritatis debitis facere odio quia architecto aperiam iusto sint amet animi excepturi quod ad nam! Laudantium sed odit non repellendus quis velit vero numquam, voluptatum similique ipsum provident vel expedita quod modi dolorem molestiae iusto. Provident maxime blanditiis laudantium voluptatem delectus totam beatae!</p>
+    </div>
   )
 }
 
--- portfolio/src/components/HomePage.jsx
+++ portfolio/src/components/HomePage.jsx
@@ -1,8 +1,98 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import reactImage from "../components/Images/react.png";
+import jsImage from "../components/Images/js.png";
+import firebaseImage from "../components/Images/firebase.jpg";
+import profileImage from "../components/Images/sibainu.jpg";
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="container text-center">
+    <h1>Shin Code</h1>
+
+    <img src={profileImage} className="profileImage" />
+
+    <p>
+      ShinCodeです。普段は個人でWeb系の情報発信をYoutubeやTwitterにて行っています。主にHTML/CSS/Javascript/Reactメインに取り扱っています。趣味は個人でプログラミングの撮影を行うことです。
+      ShinCodeです。普段は個人でWeb系の情報発信をYoutubeやTwitterにて行っています。主にHTML/CSS/Javascript/Reactメインに取り扱っています。趣味は個人でプログラミングの撮影を行うことです。
+    </p>
+
+    <section class="page-section" id="services">
+      <div className="service">
+        <div class="text-center">
+          <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
+          <h3 class="section-subheading text-muted mb-5">
+            私が作った作品一覧です
+          </h3>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="my-3">ECサイト</h4>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+              maxime quam architecto quo inventore harum ex magni, dicta
+              impedit.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="my-3">レスポンシブサイト</h4>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+              maxime quam architecto quo inventore harum ex magni, dicta
+              impedit.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="my-3">ウェブセキュリティ</h4>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+              maxime quam architecto quo inventore harum ex magni, dicta
+              impedit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="skill">
+      <div class="text-center">
+        <h1 class="title">スキル</h1>
+        <div class="row text-center">
+          <div class="col-md-4 services">
+            <img src={reactImage} />
+            <h4>React</h4>
+            <p>Reactがつかえます</p>
+          </div>
+          <div class="col-md-4 services">
+            <img src={jsImage} />
+            <h4>HTML/CSS</h4>
+            <p>HTML/CSSがつかえます</p>
+          </div>
+          <div class="col-md-4 services">
+            <img src={firebaseImage} />
+            <h4>Firebase</h4>
+            <p>Firebaseがつかえます</p>
+          </div>
+        </div>
+        <button type="button" class="btn btn-primary">
+          スキル一覧
+        </button>
+      </div>
+    </section>
+  </div>
   )
 }
 
