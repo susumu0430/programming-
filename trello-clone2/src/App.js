6d2e912293838aae60dead089e6d9aa3c17436d6
susumu1125 <susumu04301125@gmail.com>
2023-12-08 14:41:27 +0900

    追加

--- trello-clone2/src/App.js
+++ trello-clone2/src/App.js
@@ -1,9 +1,11 @@
+import { Header } from "./components/header/Header";
+
 
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ trello-clone2/src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header><h1>Trello App</h1></header>
+  )
+}
