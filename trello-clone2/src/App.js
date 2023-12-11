56e58c60bd8083785a5a97b47d8eddd7890c43f9
susumu1125 <susumu04301125@gmail.com>
2023-12-11 14:50:25 +0900

    追加

--- trello-clone2/src/App.js
+++ trello-clone2/src/App.js
@@ -1,4 +1,5 @@
 import { Header } from "./components/header/Header";
+import { TaskCards } from "./components/task/TaskCards";
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <TaskCards />
     </div>
   );
 }
---
+++ trello-clone2/src/components/task/TaskCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TaskCard } from './TaskCard'
+import { AddTaskCardButton } from './button/AddTaskCardButton'
+
+
+export const TaskCards = () => {
+  return (
+    <div>
+        <TaskCard />
+        <AddTaskCardButton />
+    </div>
+  )
+}
