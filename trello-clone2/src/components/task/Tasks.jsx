a881821be70e1cb44229c47546be840509ff8744
susumu1125 <susumu04301125@gmail.com>
2023-12-20 13:20:19 +0900

    追加

--- trello-clone2/src/components/task/Tasks.jsx
+++ trello-clone2/src/components/task/Tasks.jsx
@@ -1,38 +1,40 @@
-import React from 'react'
-import { Task } from './Task'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { Task } from "./Task";
 
 const reorder = (taskList, startIndex, endIndex) => {
-     //タスクを並び変える
-     const remove = taskList.splice(startIndex.index,1)
-     taskList.splice(endIndex,0,remove[0])
-}
+  //タスクを並び変える。
+  const remove = taskList.splice(startIndex, 1); //[2,3]
+  taskList.splice(endIndex, 0, remove[0]); //[2,1,3]
+};
 
-export const Tasks = ({taskList, setTaskList}) => {
-  const handleDragEnd = (result) => {
-    //タスクを並び変える
-  const remove = taskList.splice(result.source.index,1)
-  taskList.splice(result.destination.index,0,remove[0])
+export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDrangEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
 
-  reorder(taskList,result.source.index,result.destination.index)
-
-  setTaskList(setTaskList)
-  }
+    setTaskList(taskList);
+  };
   return (
     <div>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId='droppable'>
-          {(provided) => 
-        <div {...provided.droppableProps} ref={provided.innerRef}>
-            {taskList.map((task, index) => (
-        <div key={task.id}>
-          <Task index={index} task={task} taskList={taskList} setTaskList={setTaskList}/>
-        </div>
-      ))}
-      {provided.placeholder}
-        </div>}
-      </Droppable>
+      <DragDropContext onDragEnd={handleDrangEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {taskList.map((task, index) => (
+                <div key={task.id}>
+                  <Task
+                    index={index}
+                    task={task}
+                    taskList={taskList}
+                    setTaskList={setTaskList}
+                  />
+                </div>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </div>
-  )
-}
+  );
+};
