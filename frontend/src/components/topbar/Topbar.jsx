471b1dcfd644a2b3a6fd4f023696d88a0120d792
susumu1125 <susumu04301125@gmail.com>
2024-02-02 14:40:39 +0900

    追加

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Topbar() {
   return (
-    <div>T</div>
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>Real SNS</span>
+      </div>
+      <div className='topbarCenter'>
+        <div className="searchbar">
+          <input type="text" className='searchInput' placeholder='探し物は何ですか？'/>
+        </div>
+        </div> 
+        <div className="topbarRight">
+          <div className="topbarIconItem">1</div>
+          <div className="topbarIconItem">2</div>
+        </div>
+        <img src="/assets/person /1.jpeg" alt="" className='topbarImg'/>
+    </div>
   )
 }
