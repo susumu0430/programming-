1c79bae0828af6f51b17a9f3d5a52fbe6e01945e
susumu1125 <susumu04301125@gmail.com>
2024-03-20 10:10:49 +0900

    追加

--- api udemy/index.js
+++ api udemy/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const PORT = 5000
+app.use(express.json())
 
 app.listen(PORT, () => console.log("サーバーが起動しました"))
 
@@ -18,4 +19,23 @@ const customers = [
 
 app.get("/api/customers", (req, res) => {
     res.send(customers)
+})
+
+app.post("/api/customers", (req, res) => {
+    const customer = {
+        title: req.body.title,
+        id: customers.length + 1
+    }
+    customers.push(customer)
+    res.send(customer)
+})
+//お客様情報の更新
+app.put("/api/customers/:id",(req, res) => {
+    const customer = customers.find((c) => c.id === parseInt(req.params.id))
+    // customer.title = req.body.title
+
+    const index = customers.indexOf(customer)
+    customers.splice(index, 1)
+
+    res.send(customer)
 })
